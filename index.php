d5ff97d620e75e8abd1ec01d7289a7a617805d54
richardtoner <toner.richard@gmail.com>
2013-07-27 21:28:09 +0100

    Tags closed and navigation links now underlined when hovered over.

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 <html lang="en">
 	<head>
 		<title>Ty Gwyn | A Beautiful Holiday Cottage in Fishguard</title>
+		<meta charset="UTF-8" />
 		<!-- TODO: Remove robots meta tag when site is launched and meta data is prepared -->
-		<meta name="robots" content="noindex">
-		<meta name="description" content="">
-		<meta name="keywords" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
-		<link href="css/bootstrap-justified-nav.css" rel="stylesheet" media="screen">
-		<link href="css/gwyn.css" rel="stylesheet" media="screen">
+		<meta name="robots" content="noindex" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="author" content="Richard Toner" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen" />
+		<link href="css/bootstrap-justified-nav.css" rel="stylesheet" media="screen" />
+		<link href="css/gwyn.css" rel="stylesheet" media="screen" />
 	</head>
 	<body>
 		<div class="navbar navbar-fixed-top container">
 			<h1>Ty Gwyn</h1>
 			<ul class="nav">
-				<li><a href="the-cottage">The Cottage</a></li>
+				<li><a href="the-cottage"	>The Cottage</a></li>
 				<li><a href="gallery">Gallery</a></li>
 				<li><a href="things-to-do">Things to Do</a></li>
 				<li><a href="prices-availability">Prices &amp; Availability</a></li>
@@ -25,7 +27,7 @@
 		</div>
 		<div class="container">
 			<img title="The front of Ty Gwyn holiday cottage" src="assets/homepage_hero.jpg" alt="The wonderful white-painted facade of the Ty Gwyn holiday cottage in Fishguard" />
-			<hr>
+			<hr />
 			<p class="lead">Ty Gwyn is a very attractive, traditional stone cottage which has been renovated to a high standard with oak flooring, beams, comfortable sofas and a woodburner. It is conveniently situated on a quiet residential street a level walk from Fishguard town centre with its shops, restaurants and pubs. There is on-street parking for two cars outside. The fitted kitchen/diner leads out via a rear porch to a lovely split level garden surrounded by wooden fencing. The Pembrokeshire Coastal Path is easily accessible whilst a short downhill walk will take you to Fishguard's picturesque harbour.</p>
 		</div>
 		<script src="js/jquery-1.10.1.min.js"></script>

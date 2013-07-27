03c5b52875a629f0ff2f5f2886429631af85f882
richardtoner <toner.richard@gmail.com>
2013-07-28 00:27:18 +0100

    Contact Us page content filled in. Contains address, links to listings on other websites and an iframe of a Google Map pin-pointing the location of the property.

--- contact-us.php
+++ contact-us.php
@@ -1,5 +1,33 @@
 <?php include("partials/head.php"); ?>
 			<!-- Start page-specific content -->
-			<p class="lead">Content on the way...</p>
+			<div class="row">
+				<div class="span6">
+					<p>If you would like to make a booking enquiry, or have any questions regarding Ty Gwyn, then please feel free to send us an email or give us a call using the information provided below:</p>
+					<hr />
+					<address>
+						<strong>Ty Gwyn</strong><br />
+						Hottipass Street, Fishguard<br />
+						Pembrokeshire, SA65 9LJ, Wales<br />
+						<i class="icon-phone"></i> (+44) 07964 612941<br />
+						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+					</address>
+					<hr />
+					<p>Alternatively, you can check our availability and make a booking on the following websites:</p>
+					<ul>
+						<li>
+							<a title="Ty Gwyn listed on Owners Direct" href="http://www.ownersdirect.co.uk/walesb/W964.htm">Owners Direct</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Holiday Lettings" href="http://www.holidaylettings.co.uk/rentals/fishguard/545122">Holiday Lettings</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Trip Advisor" href="http://www.tripadvisor.co.uk/VacationRentalReview-g316015-d4500184-Ty_Gwyn_Fishguard-Fishguard_Pembrokeshire_Wales.html">Trip Advisor</a>
+						</li>
+					</ul>
+				</div>
+				<div class="span6">
+					<iframe width="460" height="344" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Hottipass+Street,+Fishguard,+SA65+9LJ&amp;aq=&amp;sll=51.926071,-4.993562&amp;sspn=0.205791,0.615921&amp;ie=UTF8&amp;hq=&amp;hnear=Hottipass+St&amp;t=m&amp;ll=51.992835,-4.971957&amp;spn=0.011099,0.019312&amp;z=15&amp;output=embed&amp;iwloc=near"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Hottipass+Street,+Fishguard,+SA65+9LJ&amp;aq=&amp;sll=51.926071,-4.993562&amp;sspn=0.205791,0.615921&amp;ie=UTF8&amp;hq=&amp;hnear=Hottipass+St&amp;t=m&amp;ll=51.992835,-4.971957&amp;spn=0.011099,0.019312&amp;z=15">View Larger Map</a></small>
+				</div>
+			</div>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

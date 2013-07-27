343f27a903bbcdd0fec510cdda032f67e21494e6
richardtoner <toner.richard@gmail.com>
2013-07-27 22:52:09 +0100

    Title and alt attributes added to footer images and links.

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
 			<hr />
 			<div class="row footer">
 				<div class="span6">
-					<img title="" src="assets/visit_wales_certification_small.jpg" alt="" />
-					<img title="" src="assets/visit_wales_walkers.jpg" alt="" />
-					<img title="" src="assets/visit_wales_cyclists.jpg" alt="" />
+					<img title="Ty Gwyn has been awarded a 4-star rating by the Welsh Tourism board" src="assets/visit_wales_certification_small.jpg" alt="Welsh Tourism Board 4 Star Rating Certificate" />
+					<img title="Walkers are welcome to stay at Ty Gwyn" src="assets/visit_wales_walkers.jpg" alt="Welsh Tourism Board walkers welcome" />
+					<img title="Cyclists are welcome to stay at Ty Gwyn" src="assets/visit_wales_cyclists.jpg" alt="Welsh Tourism Board cyclists welcome" />
 				</div>
 				<div class="span6">
 					<address>
@@ -42,7 +42,7 @@
 						Hottipass Street, Fishguard<br />
 						Pembrokeshire, SA65 9LJ, Wales<br />
 						<i class="icon-phone"></i> (+44) 07964 612941<br />
-						<i class="icon-envelope"></i> <a href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
 					</address>
 				</div>
 			</div>

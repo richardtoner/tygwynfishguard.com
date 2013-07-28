cce9f0e4dbe9771c05e8fce0b15ffa327356eedf
richardtoner <toner.richard@gmail.com>
2013-07-28 17:13:15 +0100

    Price table added to Prices and Availability page.

--- prices-availability.php
+++ prices-availability.php
@@ -1,5 +1,45 @@
 <?php include("partials/head.php"); ?>
 			<!-- Start page-specific content -->
-			<p class="lead">Content on the way...</p>
+			<table class="table table-bordered">
+				<thead>
+					<tr>
+						<th>Rental Period</th>
+						<th>Price Per Week</th>
+						<th>Min. Stay (Nights)</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1 June 2013 - 21 June 2013</td>
+						<td>GBP 490.00</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>22 June 2013 - 19 July 2013</td>
+						<td>GBP 525.00</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>20 July 2013 - 6 September 2013</td>
+						<td>GBP 595.00</td>
+						<td>7</td>
+					</tr>
+					<tr>
+						<td>7 September 2013 - 1 November 2013</td>
+						<td>GBP 420.00</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>2 November 2013 - 20 December 2013</td>
+						<td>GBP 350.00</td>
+						<td>3</td>
+					</tr>
+					<tr>
+						<td>21 December 2013 - 3 January 2014</td>
+						<td>GBP 595.00</td>
+						<td>7</td>
+					</tr>
+				</tbody>
+			</table>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

2d3996c327623f1c6b1d71515f3f00c596d8d329
richardtoner <toner.richard@gmail.com>
2013-07-29 22:07:43 +0100

    Added a holding page.

---
+++ holding.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Ty Gwyn | A Beautiful Holiday Cottage in Fishguard</title>
+		<meta charset="UTF-8" />
+		<!-- TODO: Remove robots meta tag when site is launched and meta data is prepared -->
+		<meta name="robots" content="noindex" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<meta name="author" content="Richard Toner" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link href="css/bootstrap.css" rel="stylesheet" media="screen" />
+		<link href="css/bootstrap-responsive.css" rel="stylesheet" media="screen" />
+		<link href="css/bootstrap-justified-nav.css" rel="stylesheet" media="screen" />
+		<link href="css/gwyn.css" rel="stylesheet" media="screen" />
+	</head>
+	<body>
+		<div class="navbar navbar-fixed-top container">
+			<h1><a href="/">Ty Gwyn</a></h1>
+		</div>
+		<div class="container">
+			<!-- Start page-specific content -->
+			<div class="alert">
+			  <button type="button" class="close" data-dismiss="alert">&times;</button>
+			  <strong>Website under construction!</strong><br />
+			  We've got a brand new website on the way so come back soon to see more of Ty Gwyn and find out about all the amazing things you can do with your stay in West Wales.
+			</div>
+			<div class="jumbotron">
+				<img title="The front of Ty Gwyn holiday cottage" src="assets/homepage_hero.jpg" alt="The wonderful white-painted facade of the Ty Gwyn holiday cottage in Fishguard" />
+			</div>
+			<hr />
+			<p class="lead">Ty Gwyn is a very attractive, traditional stone cottage which has been renovated to a high standard with oak flooring, beams, comfortable sofas and a woodburner. It is conveniently situated on a quiet residential street a level walk from Fishguard town centre with its shops, restaurants and pubs. There is on-street parking for two cars outside. The fitted kitchen/diner leads out via a rear porch to a lovely split level garden surrounded by wooden fencing. The Pembrokeshire Coastal Path is easily accessible whilst a short downhill walk will take you to Fishguard's picturesque harbour.</p>
+			<hr />
+			<div class="row">
+				<div class="span6">
+					<p>If you would like to make a booking enquiry, or have any questions regarding Ty Gwyn, then please feel free to send us an email or give us a call using the information provided below:</p>
+					<hr />
+					<address>
+						<strong>Ty Gwyn</strong><br />
+						Hottipass Street, Fishguard<br />
+						Pembrokeshire, SA65 9LJ, Wales<br />
+						<i class="icon-phone"></i> (+44) 07964 612941<br />
+						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+					</address>
+					<hr />
+					<p>Alternatively, you can check our availability and make a booking on the following websites:</p>
+					<ul>
+						<li>
+							<a title="Ty Gwyn listed on Owners Direct" href="http://www.ownersdirect.co.uk/walesb/W964.htm">Owners Direct</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Holiday Lettings" href="http://www.holidaylettings.co.uk/rentals/fishguard/545122">Holiday Lettings</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Trip Advisor" href="http://www.tripadvisor.co.uk/VacationRentalReview-g316015-d4500184-Ty_Gwyn_Fishguard-Fishguard_Pembrokeshire_Wales.html">Trip Advisor</a>
+						</li>
+					</ul>
+				</div>
+				<div class="span6">
+					<iframe width="100%" height="344" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Hottipass+Street,+Fishguard,+SA65+9LJ&amp;aq=&amp;sll=51.926071,-4.993562&amp;sspn=0.205791,0.615921&amp;ie=UTF8&amp;hq=&amp;hnear=Hottipass+St&amp;t=m&amp;ll=51.992835,-4.971957&amp;spn=0.011099,0.019312&amp;z=15&amp;output=embed&amp;iwloc=near"></iframe><br /><small><a href="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en&amp;geocode=&amp;q=Hottipass+Street,+Fishguard,+SA65+9LJ&amp;aq=&amp;sll=51.926071,-4.993562&amp;sspn=0.205791,0.615921&amp;ie=UTF8&amp;hq=&amp;hnear=Hottipass+St&amp;t=m&amp;ll=51.992835,-4.971957&amp;spn=0.011099,0.019312&amp;z=15">View Larger Map</a></small>
+				</div>
+			</div>
+			<!-- End page-specific content -->
+			<hr />
+			<div class="row footer">
+				<div class="span6 hidden-phone">
+					<img title="Ty Gwyn has been awarded a 4-star rating by the Welsh Tourism board" src="assets/visit_wales_certification_small.jpg" alt="Welsh Tourism Board 4 Star Rating Certificate" />
+					<img title="Walkers are welcome to stay at Ty Gwyn" src="assets/visit_wales_walkers.jpg" alt="Welsh Tourism Board walkers welcome" />
+					<img title="Cyclists are welcome to stay at Ty Gwyn" src="assets/visit_wales_cyclists.jpg" alt="Welsh Tourism Board cyclists welcome" />
+				</div>
+				<div class="span6">
+					<address>
+						<strong>Ty Gwyn</strong><br />
+						Hottipass Street, Fishguard<br />
+						Pembrokeshire, SA65 9LJ, Wales<br />
+						<i class="icon-phone"></i> (+44) 07964 612941<br />
+						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+					</address>
+				</div>
+			</div>
+		</div>
+		<script src="js/jquery-1.10.1.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>

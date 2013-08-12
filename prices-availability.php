7c95fc33333c2d85f9b61e6c2165c6ed59b679c4
richardtoner <toner.richard@gmail.com>
2013-08-12 01:39:36 +0100

    Progress on Things to Do and Prices pages.

--- prices-availability.php
+++ prices-availability.php
@@ -4,6 +4,8 @@
 				<button type="button" class="close" data-dismiss="alert">&times;</button>
 				<strong>Unfinished business!</strong> This page needs some time and love.
 			</div>
+			<div class="row-fluid">
+				<div class="span8">
 			<table class="table table-bordered">
 				<thead>
 					<tr>
@@ -13,16 +15,6 @@
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>1 June 2013 - 21 June 2013</td>
-						<td>GBP 490.00</td>
-						<td>3</td>
-					</tr>
-					<tr>
-						<td>22 June 2013 - 19 July 2013</td>
-						<td>GBP 525.00</td>
-						<td>3</td>
-					</tr>
 					<tr>
 						<td>20 July 2013 - 6 September 2013</td>
 						<td>GBP 595.00</td>
@@ -45,5 +37,30 @@
 					</tr>
 				</tbody>
 			</table>
+			
+					<p>To check availability please contact the owner using the information provided below:</p>
+					<hr />
+					<address>
+						<i class="icon-phone"></i> (+44) 07964 612941<br />
+						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+					</address>
+					<hr />
+					<p>Alternatively, you can check our availability and make a booking on the following websites:</p>
+					<ul>
+						<li>
+							<a title="Ty Gwyn listed on Owners Direct" href="http://www.ownersdirect.co.uk/walesb/W964.htm">Owners Direct</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Holiday Lettings" href="http://www.holidaylettings.co.uk/rentals/fishguard/545122">Holiday Lettings</a>
+						</li>
+						<li>
+							<a title="Ty Gwyn listed on Trip Advisor" href="http://www.tripadvisor.co.uk/VacationRentalReview-g316015-d4500184-Ty_Gwyn_Fishguard-Fishguard_Pembrokeshire_Wales.html">Trip Advisor</a>
+						</li>
+					</ul>
+				</div>
+				<div class="span4">
+					<img src="assets/captains_wheel_closeup.jpg" />
+				</div>
+			</div>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>
--- things-to-do.php
+++ things-to-do.php
@@ -4,5 +4,25 @@
 				<button type="button" class="close" data-dismiss="alert">&times;</button>
 				<strong>Unfinished business!</strong> This page needs some time and love.
 			</div>
+			<h2>Things to Do</h2>
+			<div class="row-fluid">
+				<ul class="thumbnails">
+					<li class="span4">
+						<a href="#" class="thumbnail">
+							<img src="assets/welsh_tourism_board/surfer_at_whitesands.jpg"/>
+						</a>
+					</li>
+					<li class="span4">
+						<a href="#" class="thumbnail">
+							<img src="assets/welsh_tourism_board/st_davids_cathedral.jpg"/>
+						</a>
+					</li>
+					<li class="span4">
+						<a href="#" class="thumbnail">
+							<img src="assets/welsh_tourism_board/whitesands_bay.jpg"/>
+						</a>
+					</li>
+				</ul>
+			</div>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

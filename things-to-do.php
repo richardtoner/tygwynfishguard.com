d34aa8e15d34a8d34014bcb9e1a6e09e956da612
richardtoner <toner.richard@gmail.com>
2013-08-14 01:22:55 +0100

    Cropped a few select images so that image pairs matched in height. I've also amended the gallery layout so that Bootstrap thumbnails are no longer used.

--- things-to-do.php
+++ things-to-do.php
@@ -5,133 +5,104 @@
 				<strong>Unfinished business!</strong> This page needs some time and love.
 			</div>
 			<h2>Things to Do</h2>
-			<div class="row-fluid">
-				<ul id="gallery-images" class="thumbnails">
+			<div id="gallery-images" class="row-fluid">
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/surfer_at_whitesands.jpg" title="Surfing at Whitesands Bay">
-									<img src="assets/welsh_tourism_board/surfer_at_whitesands.jpg"/>
-								</a>
-								<h3>Surfing at Whitesands Bay</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/fishguard_harbour.jpg" class="thumbnail" title="Fishguard Harbour">
-									<img src="assets/welsh_tourism_board/fishguard_harbour.jpg"/>
-								</a>
-								<h3>Fishguard Harbour</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/surfer_at_whitesands.jpg" title="Surfing at Whitesands Bay">
+								<img src="assets/welsh_tourism_board/surfer_at_whitesands.jpg"/>
+							</a>
+							<h4>Surfing at Whitesands Bay</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg" title="Walkers on the Coastal Path">
+								<img src="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg"/>
+							</a>
+							<h4>Walkers on the Coastal Path</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/children_playing_on_beach.jpg" class="thumbnail" title="Children Playing on the Beach">
-									<img src="assets/welsh_tourism_board/children_playing_on_beach.jpg"/>
-								</a>
-								<h3>Children Playing on the Beach</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/cyclists_at_porthgain.jpg" class="thumbnail" title="Cyclists at Porthgain">
-									<img src="assets/welsh_tourism_board/cyclists_at_porthgain.jpg"/>
-								</a>
-								<h3>Cyclists at Porthgain</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/children_playing_on_beach.jpg" title="Children Playing on the Beach">
+								<img src="assets/welsh_tourism_board/children_playing_on_beach.jpg"/>
+							</a>
+							<h4>Children Playing on the Beach</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/cyclists_at_porthgain.jpg" title="Cyclists at Porthgain">
+								<img src="assets/welsh_tourism_board/cyclists_at_porthgain.jpg"/>
+							</a>
+							<h4>Cyclists at Porthgain</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/oakwood_pirate_ship.jpg" class="thumbnail" title="The Pirate Ship at Oakwood Theme Park">
-									<img src="assets/welsh_tourism_board/oakwood_pirate_ship.jpg"/>
-								</a>
-								<h3>The Pirate Ship at Oakwood Theme Park</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/whitesands_bay_sunset.jpg" class="thumbnail" title="Sunset at Whitesands Bay">
-									<img src="assets/welsh_tourism_board/whitesands_bay_sunset.jpg"/>
-								</a>
-								<h3>Sunset at Whitesands Bay</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/oakwood_pirate_ship.jpg" title="The Pirate Ship at Oakwood Theme Park">
+								<img src="assets/welsh_tourism_board/oakwood_pirate_ship.jpg"/>
+							</a>
+							<h4>Pirate Ship at Oakwood Theme Park</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/oakwood_drenched.jpg" title="Drenched at Oakwood Theme Park">
+								<img src="assets/welsh_tourism_board/oakwood_drenched.jpg"/>
+							</a>
+							<h4>Drenched at Oakwood Theme Park</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/oakwood_drenched.jpg" class="thumbnail" title="Drenched at Oakwood Theme Park">
-									<img src="assets/welsh_tourism_board/oakwood_drenched.jpg"/>
-								</a>
-								<h3>Drenched at Oakwood Theme Park</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/golfing_pembrokeshire.jpg" class="thumbnail" title="Golfing in Pembrokeshire">
-									<img src="assets/welsh_tourism_board/golfing_pembrokeshire.jpg"/>
-								</a>
-								<h3>Golfing in Pembrokeshire</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/whitesands_bay_sunset.jpg" title="Sunset at Whitesands Bay">
+								<img src="assets/welsh_tourism_board/whitesands_bay_sunset.jpg"/>
+							</a>
+							<h4>Sunset at Whitesands Bay</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/golfing_pembrokeshire.jpg" title="Golfing in Pembrokeshire">
+								<img src="assets/welsh_tourism_board/golfing_pembrokeshire.jpg"/>
+							</a>
+							<h4>Golfing in Pembrokeshire</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg" class="thumbnail" title="Walkers on the Coastal Path">
-									<img src="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg"/>
-								</a>
-								<h3>Walkers on the Coastal Path</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg" class="thumbnail" title="Coasteerers off to the Blue Lagoon">
-									<img src="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg"/>
-								</a>
-								<h3>Coasteerers off to the Blue Lagoon</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/strumble_head_lighthouse.jpg" title="Strumble Head Lighthouse">
+								<img src="assets/welsh_tourism_board/strumble_head_lighthouse.jpg"/>
+							</a>
+							<h4>Strumble Head Lighthouse</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg" title="Coasteerers off to the Blue Lagoon">
+								<img src="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg"/>
+							</a>
+							<h4>Coasteerers off to the Blue Lagoon</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/st_davids_cathedral.jpg" class="thumbnail" title="St David's Cathedral">
-									<img src="assets/welsh_tourism_board/st_davids_cathedral.jpg"/>
-								</a>
-								<h3>St David's Cathedral</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/whitesands_bay.jpg" class="thumbnail" title="Whitesands Bay">
-									<img src="assets/welsh_tourism_board/whitesands_bay.jpg"/>
-								</a>
-								<h3>Whitesands Bay</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/st_davids_cathedral.jpg" title="St David's Cathedral">
+								<img src="assets/welsh_tourism_board/st_davids_cathedral.jpg"/>
+							</a>
+							<h4>St David's Cathedral</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/whitesands_bay.jpg" title="Whitesands Bay">
+								<img src="assets/welsh_tourism_board/whitesands_bay.jpg"/>
+							</a>
+							<h4>Whitesands Bay</h4>
+						</div>
 					</div>
 					<div class="row-fluid">
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/strumble_head_lighthouse.jpg" class="thumbnail" title="Strumble Head Lighthouse">
-									<img src="assets/welsh_tourism_board/strumble_head_lighthouse.jpg"/>
-								</a>
-								<h3>Strumble Head Lighthouse</h3>
-							</div>
-						</li>
-						<li class="span6">
-							<div class="thumbnail">
-								<a href="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg" class="thumbnail" title="The Blue Lagoon at Abereiddy">
-									<img src="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg"/>
-								</a>
-								<h3>The Blue Lagoon at Abereiddy</h3>
-							</div>
-						</li>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/fishguard_harbour.jpg" title="Fishguard Harbour">
+								<img src="assets/welsh_tourism_board/fishguard_harbour.jpg"/>
+							</a>
+							<h4>Fishguard Harbour</h4>
+						</div>
+						<div class="span6 gallery-image">
+							<a href="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg" title="The Blue Lagoon at Abereiddy">
+								<img src="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg"/>
+							</a>
+							<h4>The Blue Lagoon at Abereiddy</h4>
+						</div>
 					</div>
 				</ul>
 			</div>

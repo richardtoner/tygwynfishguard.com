28dafdb28478e4f0c5c35b6e0e68f84e22878770
richardtoner <toner.richard@gmail.com>
2013-08-20 01:00:56 +0100

    The Cottage has been restructured and images have been added.

--- the-cottage.php
+++ the-cottage.php
@@ -5,55 +5,76 @@
 				<strong>Unfinished business!</strong> This page needs some time and love.
 			</div>
 			<h2>The Cottage</h2>
-			<div class="row-fluid">
-				<div class="span7">
+			<p class="lead">[A little description about The Cottage]</p>
+			<div id="gallery-images" class="row-fluid">
+				<div class="row-fluid">
+					<div class="span6">
+						<h3>Living Room</h3>
+						<p>The large living room is very comfortable with two large sofas, chaise, woodburner, plasma TV with Sky, DVD player and oak flooring. A selection of books and childrens games are contained within the oak sideboard. It will easily seat six in comfort.</p>
+						<div class="gallery-image">
+							<a href="assets/lounge.jpg" title="Lounge">
+								<img class="img-rounded" src="assets/lounge.jpg"/>
+							</a>
+						</div>
+					</div>
+					<div class="span6">
+						<h3>Kitchen</h3>
+						<p>Large fitted kitchen/diner equipped with electric oven, gas hob, fridge/freezer, double sink, washing machine, microwave and toaster. A large traditional farmhouse table and chairs provides a cosy space for communal dining.</p>
+						<div class="gallery-image">
+							<a href="assets/kitchen.jpg" title="Kitchen">
+								<img class="img-rounded" src="assets/kitchen.jpg"/>
+							</a>
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
 					<h3>Bedrooms</h3> 
 					<p>Bedrooms 1 and 2 both have double beds whilst bedroom three is a quirky split level crogloft equipped with two single beds. Whilst the twin beds can accommodate adults, the upper level has limited headroom and this bedroom is very popular with children.</p>
-					<h3>Bathrooms</h3>
-					<p>Modern, tiled, main family bathroom with bath, power shower over, toilet and washbasin. There is also a new second cloakroom and wash handbasin next to the twin bedroom.</p>
 				</div>
-				<div class="span5">
-					<img src="assets/bedroom_1.jpg" />
+				<div class="row-fluid">
+					<div class="span6 gallery-image">
+						<a href="assets/bedroom_1.jpg" title="Bedroom 1">
+							<img class="img-rounded" src="assets/bedroom_1.jpg"/>
+						</a>
+					</div>
+					<div class="span6 gallery-image">
+						<a href="assets/bedroom_2.jpg" title="Bedroom 2">
+							<img class="img-rounded" src="assets/bedroom_2.jpg"/>
+						</a>
+					</div>
 				</div>
-			</div>
-			<div class="row-fluid">
-				<div class="span5">
-					<img src="assets/kitchen.jpg" />
+				<div class="row-fluid">
+					<div class="span6">
+						<h3>Bathrooms</h3>
+						<p>Modern, tiled, main family bathroom with bath, power shower over, toilet and washbasin. There is also a new second cloakroom and wash handbasin next to the twin bedroom.</p>
+						<div class="gallery-image">
+							<a href="assets/bathroom.jpg" title="Bathroom">
+								<img class="img-rounded" src="assets/bathroom.jpg"/>
+							</a>
+						</div>
+					</div>
+					<div class="span6">
+						<h3>Garden</h3>
+						<p>[Description of garden required]</p>
+						<div class="gallery-image">
+							<a href="assets/garden_overview.jpg" title="Garden">
+								<img class="img-rounded" src="assets/garden_overview.jpg"/>
+							</a>
+						</div>
+					</div>
 				</div>
-				<div class="span7">
-					<h3>Kitchen</h3>
-					<p>Large fitted kitchen/diner equipped with electric oven, gas hob, fridge/freezer, double sink, washing machine, microwave and toaster. A large traditional farmhouse table and chairs provides a cosy space for communal dining.</p>
-
-					<h3>Utility Room</h3>
-					<p>Rear porch with Velux window, access to rear garden where there is a second upright fridge/freezer (useful for drinks), ironing board and iron.</p>
-				</div>
-			</div>
-			<div class="row-fluid">
-				<div class="span7">
-					<h3>Living Rooms</h3>
-					<p>The large living room is very comfortable with two large sofas, chaise, woodburner, plasma TV with Sky, DVD player and oak flooring. A selection of books and childrens games are contained within the oak sideboard. It will easily seat six in comfort.</p>
-					<h3>Additional Information</h3>
-					<p>Bed linen and tea towels are provided with the property. However, towels for personal use are not. There is a small selection of kitchen basics i.e. condiments also available for guests use.</p>
-				</div>
-				<div class="span5">
-					<img src="assets/lounge.jpg" />
+				<div class="row-fluid">
+					<div class="span6">
+						<h3>Utility Room</h3>
+						<p>Rear porch with Velux window, access to rear garden where there is a second upright fridge/freezer (useful for drinks), ironing board and iron.</p>
+					</div>
+					<div class="span6">
+						<h3>Additional Information</h3>
+						<p>Bed linen and tea towels are provided with the property. However, towels for personal use are not. There is a small selection of kitchen basics i.e. condiments also available for guests use.</p>
+					</div>
 				</div>
 			</div>
 			<hr />
-			<h3>Amenities/Facilities</h3>
-			<ul>
-				<li>Barbecue</li>
-				<li>Garden</li> 
-				<li>Fridge/Freezer</li>
-				<li>Hob/Stove</li>
-				<li>Iron</li>
-				<li>Microwave</li>
-				<li>Oven</li>
-				<li>Washing Machine</li> 
-				<li>Central Heating</li>
-				<li>Cot</li>
-				<li>High Chair</li>
-				<li>Satellite TV</li>
-			</ul>
+			<?php include("partials/blueimp-gallery.html"); ?>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

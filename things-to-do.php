67eb1a0e984a4204f9d508ddec1ba286a72b9019
richardtoner <toner.richard@gmail.com>
2013-08-22 00:45:01 +0100

    Removed Fishguard text from Things to Do and added a lead paragraph.

--- things-to-do.php
+++ things-to-do.php
@@ -5,6 +5,7 @@
 				<strong>Unfinished business!</strong> This page needs some time and love.
 			</div>
 			<h2>Things to Do</h2>
+			<p class="lead">Ty Gwyn is fortunately situated in one of the loveliest counties in Britain and there are numerous places to visit within walking or driving distance.</p>
 			<div id="gallery-images" class="row-fluid">
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
@@ -105,20 +106,6 @@
 					</div>
 				</div>
 			</div>
-			<h3>Fishguard</h3>
-			<p>Fishguard consists of two separate and distinct areas – Lower Town, ie the area around the harbour and Upper Town – where Ty Gwyn is situated.</p>
-			<p>Lower Town (turn right out of the cottage, first right and first right again and down the steep hill) is very picturesque and is often used as a location for film sets.   Worth a walk but the hill backup is a bit of a killer – we usually catch the bus back to the Square!   Alternatively you can follow the signs for ‘Marine Walk’ which is a pretty walk around the headland and will take you back to the town centre.  Lower Town was used as the venue for the filming of Moby Dick and Under Milk Wood – you will see some memorabilia on the walls of the Ship Inn there.</p>
-			<p>Upper Town – turn right out of the cottage, right at the end and then left and you are in Market Square, the town centre.  There’s a good selection of shops here as you would expect in a busy market town.</p>
-			<p>There’s a farmer’s market every Thursday morning in the Town Hall in Market Square where they sell meat, fish, cheeses, bread etc.  It’s worth a visit for good local produce.   Also every other Saturday – keep an eye out for placards around the town.    You can enter in the back way at the end of the street – it’s a short cut to the Square.</p>
-			<p>You are spoilt for choice for pubs in Fishguard; within walking distance (5 – 10mins) are quite a few and here’s a quick run down:</p>
-			<ul>
-				<li>The Cambrian – Corner of the street.  Popular with locals.  Mine hosts Graham and June.</li>
-				<li>Royal Oak – Market Sq. Historical, famous pub and popular with tourists.  Where the surrender of the last invasion of Britain was signed.</li>
-				<li>Fishguard Arms – tiny little pub with a fire.  Instead of turning left into Market Square opposite 3 Main St turn right and it’s about 50ft up, just after the ice cream parlour.  Beware the organic cider.   Bit tatty.</li>
-				<li>The Coach House – Mixed age group and popular on weekends, more for the younger element.</li>
-				<li>Fishguard and Goodwick Rugby Club (near the Post Office)– exactly as it says but a good 15 min walk.  Great if you want to watch an international!  Cheap beer during matches.  Well worth a visit – non members are welcome.</li>
-				<li>The Ship Inn – Lower Town. Turn right from the cottage, first right and right at the end and follow the long hill down.  (Buses on the hour back up the hill – mine host Richard has the time-table).  Full of characters, historic and a firm favourite not to be missed.</li>
-			</ul>
 			<h3>Places To Go</h3>
 			<p>You are fortunately situated in one of the loveliest counties in Britain and there are numerous places to visit within walking or driving distance.  Just a brief selection is:</p>
 			<p>Lower Town.  (See ‘Ship Inn’ overleaf.) If you are feeling energetic this is a lovely short walk with pretty views.   Turn right out of Ty Gwyn, first right and right again at the end.   Go down the steep hill to the harbour at Lower Town.  Stop at the historic Ship Inn for a drink with mine hosts Richard and Jan.   It’s like a cosy front room.  See photos on the wall of Gregory Peck when Moby Dick was filmed there and also Richard Burton whilst filming Under Milkwood.  If you come back the same way it’s a real killer, so I suggest following the water all the way around until you see a Tarmac slope on the left – you can’t miss it as you can’t walk any further.  You then come up the Slade which brings you back to the centre of Fishguard.  It’s still steep but a bit more stretched out and not nearly as bad as the main road.</p>

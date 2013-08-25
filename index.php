1000ae2d3d6d06309bcb479adf108369e6545eb5
richardtoner <toner.richard@gmail.com>
2013-08-25 21:03:42 +0100

    Added spotlights to homepage and altered divider spacing on homepage.

--- index.php
+++ index.php
@@ -5,5 +5,26 @@
 			</div>
 			<hr />
 			<p class="lead">Ty Gwyn is a very attractive, traditional stone cottage which has been renovated to a high standard with oak flooring, beams, comfortable sofas and a woodburner. It is a 3 bedroom property conveniently situated on a quiet residential street a level walk from Fishguard town centre with its shops, restaurants and pubs. There is on-street parking for two cars outside. The fitted kitchen/diner leads out via a rear porch to a lovely split level garden surrounded by wooden fencing. The Pembrokeshire Coastal Path is easily accessible whilst a short downhill walk will take you to Fishguard's picturesque harbour.</p>
+			<hr />
+			<div class="row-fluid spotlight-container">
+				<div class="span4">
+					<img class="img-circle" src="assets/spotlights/lounge_spotlight.jpg" />
+					<h3>The Cottage</h3>
+					<p class="text-center">[Some call to action text required]</p>
+					<p class="text-center"><a class="btn" href="the-cottage">Read more »</a></p>
+				</div>
+				<div class="span4">
+					<img class="img-circle" src="assets/spotlights/strumble_head_spotlight.jpg" />
+					<h3>Things to Do</h3>
+					<p class="text-center">[Some call to action text required]</p>
+					<p class="text-center"><a class="btn" href="things-to-do">Read more »</a></p>
+				</div>
+				<div class="span4">
+					<img class="img-circle" src="assets/spotlights/map_spotlight.jpg" />
+					<h3>Contact Us</h3>
+					<p class="text-center">[Some call to action text required]</p>
+					<p class="text-center"><a class="btn" href="contact-us">Read more »</a></p>
+				</div>
+			</div>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

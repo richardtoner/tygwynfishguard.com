8d619c5f9a803fb04c9081c95be5c9d962b80332
richardtoner <toner.richard@gmail.com>
2013-08-26 01:11:58 +0100

    Things to do finished.

--- things-to-do.php
+++ things-to-do.php
@@ -1,36 +1,52 @@
 <?php include("partials/head.php"); ?>
 			<!-- Start page-specific content -->
-			<div class="alert alert-error">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-				<strong>Unfinished business!</strong> This page needs some time and love.
-			</div>
 			<h2>Things to Do</h2>
 			<p class="lead">Ty Gwyn is fortunately situated in one of the loveliest counties in Britain and there are numerous places to visit within walking or driving distance.</p>
 			<div id="gallery-images" class="row-fluid">
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/surfer_at_whitesands.jpg" title="Surfing at Whitesands Bay">
-							<img src="assets/welsh_tourism_board/surfer_at_whitesands.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/surfer_at_whitesands.jpg"/>
 						</a>
 						<h4>Surfing at Whitesands Bay</h4>
 					</div>
 					<div class="span6 gallery-image">
-						<a href="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg" title="Walkers on the Coastal Path">
-							<img src="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg"/>
+						<a href="assets/welsh_tourism_board/whitesands_bay.jpg" title="Whitesands Bay">
+							<img class="img-rounded" src="assets/welsh_tourism_board/whitesands_bay.jpg"/>
 						</a>
-						<h4>Walkers on the Coastal Path</h4>
+						<h4>Whitesands Bay</h4>
 					</div>
 				</div>
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
+						<a href="assets/welsh_tourism_board/st_davids_cathedral.jpg" title="St David's Cathedral">
+							<img class="img-rounded" src="assets/welsh_tourism_board/st_davids_cathedral.jpg"/>
+						</a>
+						<h4>St David's Cathedral</h4>
+					</div>
+					<div class="span6 gallery-image">
+						<a href="assets/welsh_tourism_board/whitesands_bay_sunset.jpg" title="Sunset at Whitesands Bay">
+							<img class="img-rounded" src="assets/welsh_tourism_board/whitesands_bay_sunset.jpg"/>
+						</a>
+						<h4>Sunset at Whitesands Bay</h4>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<h5>Whitesands Bay</h5>
+					<p>Voted the best beach in Britain several times with good reason. It is regarded as one of the best surfing beaches in the country and has plenty of facilities including a café. A must-visit when the sun is shining!</p>
+					<h5>St David’s</h5>
+					<p>No visit would be complete without a visit to this fabulous tiny city, the smallest in the UK. The cathedral there is simply breathtaking and dates back to the 11th Century.</p>
+				</div>
+				<div class="row-fluid">
+					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/children_playing_on_beach.jpg" title="Children Playing on the Beach">
-							<img src="assets/welsh_tourism_board/children_playing_on_beach.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/children_playing_on_beach.jpg"/>
 						</a>
 						<h4>Children Playing on the Beach</h4>
 					</div>
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/cyclists_at_porthgain.jpg" title="Cyclists at Porthgain">
-							<img src="assets/welsh_tourism_board/cyclists_at_porthgain.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/cyclists_at_porthgain.jpg"/>
 						</a>
 						<h4>Cyclists at Porthgain</h4>
 					</div>
@@ -38,90 +54,80 @@
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/oakwood_pirate_ship.jpg" title="The Pirate Ship at Oakwood Theme Park">
-							<img src="assets/welsh_tourism_board/oakwood_pirate_ship.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/oakwood_pirate_ship.jpg"/>
 						</a>
 						<h4>Pirate Ship at Oakwood Theme Park</h4>
 					</div>
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/oakwood_drenched.jpg" title="Drenched at Oakwood Theme Park">
-							<img src="assets/welsh_tourism_board/oakwood_drenched.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/oakwood_drenched.jpg"/>
 						</a>
 						<h4>Drenched at Oakwood Theme Park</h4>
 					</div>
 				</div>
 				<div class="row-fluid">
-					<div class="span6 gallery-image">
-						<a href="assets/welsh_tourism_board/whitesands_bay_sunset.jpg" title="Sunset at Whitesands Bay">
-							<img src="assets/welsh_tourism_board/whitesands_bay_sunset.jpg"/>
-						</a>
-						<h4>Sunset at Whitesands Bay</h4>
-					</div>
-					<div class="span6 gallery-image">
-						<a href="assets/welsh_tourism_board/golfing_pembrokeshire.jpg" title="Golfing in Pembrokeshire">
-							<img src="assets/welsh_tourism_board/golfing_pembrokeshire.jpg"/>
-						</a>
-						<h4>Golfing in Pembrokeshire</h4>
-					</div>
+					<h5>Oakwood Theme Park</h5>
+					<p>Oakwood Theme Park is a theme park situated a half hour drive from Fishguard, which attracts over 400,000 visitors each year. It has over 30 rides and attractions varying from small family attractions to large rollercoasters suited to thrillseekers.</p>
+					<h5>Cwm Yr Eglwys</h5>
+					<p>A personal favourite if you want an old fashioned day on the beach with the kids. No shops or amusements – just a small, sheltered beach (where you can keep an eye on the little ones), rock pools (for crabbing) and a boats slipway.</p>
 				</div>
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/strumble_head_lighthouse.jpg" title="Strumble Head Lighthouse">
-							<img src="assets/welsh_tourism_board/strumble_head_lighthouse.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/strumble_head_lighthouse.jpg"/>
 						</a>
 						<h4>Strumble Head Lighthouse</h4>
 					</div>
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg" title="Coasteerers off to the Blue Lagoon">
-							<img src="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/coasteering_blue_lagoon.jpg"/>
 						</a>
 						<h4>Coasteerers off to the Blue Lagoon</h4>
 					</div>
 				</div>
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
-						<a href="assets/welsh_tourism_board/st_davids_cathedral.jpg" title="St David's Cathedral">
-							<img src="assets/welsh_tourism_board/st_davids_cathedral.jpg"/>
+						<a href="assets/welsh_tourism_board/golfing_pembrokeshire.jpg" title="Golfing in Pembrokeshire">
+							<img class="img-rounded" src="assets/welsh_tourism_board/golfing_pembrokeshire.jpg"/>
 						</a>
-						<h4>St David's Cathedral</h4>
+						<h4>Golfing in Pembrokeshire</h4>
 					</div>
 					<div class="span6 gallery-image">
-						<a href="assets/welsh_tourism_board/whitesands_bay.jpg" title="Whitesands Bay">
-							<img src="assets/welsh_tourism_board/whitesands_bay.jpg"/>
+						<a href="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg" title="Walkers on the Coastal Path">
+							<img class="img-rounded" src="assets/welsh_tourism_board/walkers_on_the_pembrokeshire_coast_path.jpg"/>
 						</a>
-						<h4>Whitesands Bay</h4>
+						<h4>Walkers on the Pembrokeshire Coastal Path</h4>
 					</div>
+				</div>
+				<div class="row-fluid">
+					<h5>Bluestone</h5>
+					<p>If the weather is wet and you are looking for somewhere to take the kids you can’t go far wrong with a day in the pool at Bluestone. It’s situated just outside Narberth and well sign-posted. The indoor lagoon is great fun with water slides and a lazy river.</p>
+					<h5>Pembrokeshire Coastal Path</h5>
+					<p>Maintained by the National Trust with absolutely wonderful views and fresh air. The Puffin Shuttle and Poppit Rocket bus services run from Market Square and will drop you off at various points so that you can walk back.</p>
 				</div>
 				<div class="row-fluid">
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/fishguard_harbour.jpg" title="Fishguard Harbour">
-							<img src="assets/welsh_tourism_board/fishguard_harbour.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/fishguard_harbour.jpg"/>
 						</a>
 						<h4>Fishguard Harbour</h4>
 					</div>
 					<div class="span6 gallery-image">
 						<a href="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg" title="The Blue Lagoon at Abereiddy">
-							<img src="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg"/>
+							<img class="img-rounded" src="assets/welsh_tourism_board/blue_lagoon_abereiddy.jpg"/>
 						</a>
 						<h4>The Blue Lagoon at Abereiddy</h4>
 					</div>
 				</div>
+				<div class="row-fluid">
+					<h5>Solva</h5>
+					<p>A picturesque little town with a beautiful harbour and interesting shops and pubs. Slow down on the steep hill and turn into the car park before stopping for a snack in The Harbour pub overlooking the bay.</p>
+					<h5>Llys y Fran Country Park</h5>
+					<p>Lovely park with free entry.  Great if you want to take a picnic or kick a ball about with the kids.  Loads of woodland paths and you can hire bicycles there for a small fee and cycle all the way around.</p>
+					<h5>That's Not all!</h5>
+					<p>What you see on this page is only a taste of what can be done in Pembrokeshire. When you arrive at Ty Gwyn you will find a welcome pack that will tell you about the best local eats and attractions.</p>
+				</div>
 			</div>
-			<h3>Places To Go</h3>
-			<p>You are fortunately situated in one of the loveliest counties in Britain and there are numerous places to visit within walking or driving distance.  Just a brief selection is:</p>
-			<p>Lower Town.  (See ‘Ship Inn’ overleaf.) If you are feeling energetic this is a lovely short walk with pretty views.   Turn right out of Ty Gwyn, first right and right again at the end.   Go down the steep hill to the harbour at Lower Town.  Stop at the historic Ship Inn for a drink with mine hosts Richard and Jan.   It’s like a cosy front room.  See photos on the wall of Gregory Peck when Moby Dick was filmed there and also Richard Burton whilst filming Under Milkwood.  If you come back the same way it’s a real killer, so I suggest following the water all the way around until you see a Tarmac slope on the left – you can’t miss it as you can’t walk any further.  You then come up the Slade which brings you back to the centre of Fishguard.  It’s still steep but a bit more stretched out and not nearly as bad as the main road.</p>
-			<p>Pwllgwaelodd – This is the nearest beach to the cottage.    Turn right at the end of the road and right again through Lower Town towards Cardigan.  You will see a sign for Pwllgwaelod after about five minutes.</p>
-			<p>Bluestone – If the weather is wet and you are looking for somewhere to take the kids you can’t go far wrong with a day in the pool at Bluestone.  It’s situated just outside Narberth and well sign-posted.  The indoor lagoon is great fun with water sides, lazy river etc.</p>
-			<p>Pembrokeshire Coastal Path – Maintained by the National Trust and wonderful views and fresh air.   The Puffin Shuttle and Poppit Rocket bus services run from Market Sq and will drop you off at various points so that you can walk back.</p>
-			<p>St David’s – No visit would be complete without a visit to this fabulous tiny city, the smallest in the UK.  The cathedral there is simply breathtaking and dates back to the 11th Century.</p>
-			<p>Solva – Keep going on the same road after passing through St David’s for a further few miles until you come to the picturesque little town of Solva with its beautiful harbour and interesting shops and pubs.  Slow down on the steep hill and turn into the car park before stopping for a snack in The Harbour pub overlooking the bay.</p>
-			<p>Newgale  -  Again proceed in the same direction after leaving Solva and after ten minutes or so you will come to Newgale, a huge, windswept beach, of mainly pebbles.   Very popular with serious windsurfers and well worth a brief visit and a cuppa in the beach café.</p>
-			<p>Haverfordwest. An interesting little town and the nearest place for good shopping (called ‘Harford’ locally).  About 9 miles from Fishguard.</p>
-			<p>Miranda’s Preserves.   Take the road out of Fishguard and along the Parrog towards the ferry terminal.   Go straight over to Goodwick and up the steep hill.  Keep going right through Goodwick on the same road as if you were going towards Strumble Head.   About half a mile after Goodwick you will see a large white house on the left with a table full of fare and a trust box outside.  Lovely pickles and especially nice lemon curd.  She has won many Taste of Wales awards for her fabulous, natural produce.  Great views of the harbour on the way back.</p>
-			<p>Porthgain – Well worth a visit just for a look even if you are not eating at The Sloop.  Watch the kids by the edge as it’s a steep fall into the harbour and no railings!</p>
-			<p>Whitesands Beach.  A bit further on the same road and about two miles before St David’s.  If you haven’t been there it’s worth a visit, especially in summer.  Voted the best beach in Britain several times.  Car park fee about £3 per day.  Beach café.</p>
-			<p>Cwm Yr Eglwys.  A personal favourite if you want an old fashioned day on the beach with the kids.  No shops or amusements – just a small, sheltered beach (where you can keep an eye on the little ones), rock pools (for crabbing) and a boats slipway.  Limited parking so get there early if the sun is out.  Leave the cottage and head right and right again towards Cardigan.   After a few miles keep an eye out for a left hand turn sign-posted ‘Cwm Yr Elgwys’.  Turn left and follow the country road for about a mile – actually the end of the road is a dead end into the car park.  Parking about £2.00 all day.  Take sandwiches and drinks with you if you are going for the day.  [Incidentally one other point of interest are the remains of St Brynach’s Church situated just before the beach – it was destroyed in a great storm in the 16th century].</p>
-			<p>Llys y Fran Country Park.   Lovely park with free entry.  Great if you want to take a picnic or kick a ball about with the kids.  Loads of woodland paths and you can hire bicycles there for a small fee and cycle all the way around – about 7 miles.  However having done this recently wouldn’t recommend it unless you are fit – it’s sweetly described as ‘challenging’!   Drive left from the cottage towards Maenclochog. This road takes you through the Gwaun Valley. Just keep going on the same road following signs for Narbeth – lovely views – until you eventually come to a T junction with Llys Y Fran s/p on the left.</p>
-			<p>Th Dyffrun Arms – Known to all and sundry as ‘Bessie’s’.  This is a pub situated in the heart of the Gwaun Valley (‘Cwm Gwaun’) about two miles from the cottage and is famous for the (very) old Welsh speaking lady who serves beer in jugs in what passes for her living room.  Apparently Bessie’s is rated above St David’s Cathedral for ‘Things not to Miss’ in Pembrokeshire!!  Living history!</p>
 			<?php include("partials/blueimp-gallery.html"); ?>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

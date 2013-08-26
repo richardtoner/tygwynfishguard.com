8df7c86b8c160958a11676ca82983b230c674c03
richardtoner <toner.richard@gmail.com>
2013-08-26 02:49:45 +0100

    Images picked for gallery.

--- gallery.php
+++ gallery.php
@@ -1,8 +1,97 @@
 <?php include("partials/head.php"); ?>
 			<!-- Start page-specific content -->
-			<div class="alert alert-error">
-				<button type="button" class="close" data-dismiss="alert">&times;</button>
-				<strong>Unfinished business!</strong> This page needs some time and love.
+			<h2>Gallery</h2>
+			<p class="lead">Welcome to our gallery! To get a closer look at the images just click on any one of them to open the full-screen view mode.</p>
+			<div id="gallery-images" class="row-fluid">
+				<div class="row-fluid">
+					<div class="span3 gallery-image">
+						<a href="assets/outside_2.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/outside_2.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/outside.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/outside.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/garden_overview.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/garden_overview.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/garden_blossom_tree.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/garden_blossom_tree.jpg"/>
+						</a>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span3 gallery-image">
+						<a href="assets/patio_log_store.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/patio_log_store.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/back_porch.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/back_porch.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/lounge.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/lounge.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/lounge_2.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/lounge_2.jpg"/>
+						</a>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span3 gallery-image">
+						<a href="assets/woodburner.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/woodburner.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/kitchen.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/kitchen.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/kitchen_2.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/kitchen_2.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/bedroom_1.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/bedroom_1.jpg"/>
+						</a>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span3 gallery-image">
+						<a href="assets/bedroom_2.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/bedroom_2.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/bedroom_3_above.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/bedroom_3_above.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/bedroom_3_below.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/bedroom_3_below.jpg"/>
+						</a>
+					</div>
+					<div class="span3 gallery-image">
+						<a href="assets/bathroom.jpg" title="">
+							<img alt="" class="img-rounded" src="assets/bathroom.jpg"/>
+						</a>
+					</div>
+				</div>
 			</div>
+			<?php include("partials/blueimp-gallery.html"); ?>
 			<!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>

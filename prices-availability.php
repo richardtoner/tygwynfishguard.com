d03461ea0b00a12cb4e70f123feca69816898d94
richardtoner <toner.richard@gmail.com>
2014-07-30 23:39:36 +0100

    Knock knock. Who's there? White space police.

--- prices-availability.php
+++ prices-availability.php
@@ -5,44 +5,44 @@ $meta_description = "From as little as &pound;350 per week you can relax in our 
 $meta_keywords = "ty gwyn, fishguard, pembrokeshire, west wales, wales, reasonable, affordable, accommodation, self-catered, holiday cottage, holiday rentals, 3 bedrooms, garden";
 include("partials/head.php"); 
 ?>
-			<!-- Start page-specific content -->
-			<h2>Prices &amp; Availability</h2>
-			<div class="row-fluid">
-				<div class="span8">
-					<table class="table table-bordered">
-						<thead>
-							<tr>
-								<th>Rental Period</th>
-								<th>Price Per Week</th>
-								<th>Min. Stay (Nights)</th>
-							</tr>
-						</thead>
-						<tbody>
-							<tr>
-								<td>19 July 2014 - 5 September 2014 (Summer Holidays)</td>
-								<td>GBP 595.00</td>
-								<td>7</td>
-							</tr>
-							<tr>
-								<td>6 September 2014 - 24 October 2014</td>
-								<td>GBP 420.00</td>
-								<td>3</td>
-							</tr>
-							<tr>
-								<td>25 October 2014 - 31 October 2014 (Half Term)</td>
-								<td>GBP 495.00</td>
-								<td>7</td>
-							</tr>
-							<tr>
-								<td>1 November 2014 - 19 December 2014</td>
-								<td>GBP 350.00</td>
-								<td>3</td>
-							</tr>
-							<tr>
-								<td>20 December 2014 - 2 January 2015 (Christmas and New Year)</td>
-								<td>GBP 595.00</td>
-								<td>7</td>
-							</tr>
+            <!-- Start page-specific content -->
+            <h2>Prices &amp; Availability</h2>
+            <div class="row-fluid">
+                <div class="span8">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Rental Period</th>
+                                <th>Price Per Week</th>
+                                <th>Min. Stay (Nights)</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>19 July 2014 - 5 September 2014 (Summer Holidays)</td>
+                                <td>GBP 595.00</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>6 September 2014 - 24 October 2014</td>
+                                <td>GBP 420.00</td>
+                                <td>3</td>
+                            </tr>
+                            <tr>
+                                <td>25 October 2014 - 31 October 2014 (Half Term)</td>
+                                <td>GBP 495.00</td>
+                                <td>7</td>
+                            </tr>
+                            <tr>
+                                <td>1 November 2014 - 19 December 2014</td>
+                                <td>GBP 350.00</td>
+                                <td>3</td>
+                            </tr>
+                            <tr>
+                                <td>20 December 2014 - 2 January 2015 (Christmas and New Year)</td>
+                                <td>GBP 595.00</td>
+                                <td>7</td>
+                            </tr>
                             <tr>
                                 <td>3 January 2015 – 13 February 2015</td>
                                 <td>GBP 395.00</td>
@@ -113,31 +113,31 @@ include("partials/head.php");
                                 <td>GBP 645.00</td>
                                 <td>7</td>
                             </tr>
-						</tbody>
-					</table>
-					<p>To check availability please contact the owner using the information provided below:</p>
-					<hr />
-					<address>
-						<i class="icon-phone"></i> (+44) 07964 612941<br />
-						<i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
-					</address>
-					<hr />
-					<p>Alternatively, you can check our availability and make a booking on the following websites:</p>
-					<ul>
-						<li>
-							<a title="Ty Gwyn listed on Owners Direct" href="http://www.ownersdirect.co.uk/wales/W964.htm">Owners Direct</a>
-						</li>
-						<li>
-							<a title="Ty Gwyn listed on Holiday Lettings" href="http://www.holidaylettings.co.uk/rentals/fishguard/545122">Holiday Lettings</a>
-						</li>
-						<li>
-							<a title="Ty Gwyn listed on Trip Advisor" href="http://www.tripadvisor.co.uk/VacationRentalReview-g316015-d4500184-Ty_Gwyn_Fishguard-Fishguard_Pembrokeshire_Wales.html">Trip Advisor</a>
-						</li>
-					</ul>
-				</div>
-				<div class="span4">
-					<img title="The woodburner is perfect for a cosy Winter's night in" alt="A traditional woodburner in Ty Gwyn cottage" class="img-rounded" src="assets/cottage/woodburner_closeup.jpg" />
-				</div>
-			</div>
-			<!-- End page-specific content -->
+                        </tbody>
+                    </table>
+                    <p>To check availability please contact the owner using the information provided below:</p>
+                    <hr />
+                    <address>
+                        <i class="icon-phone"></i> (+44) 07964 612941<br />
+                        <i class="icon-envelope"></i> <a title="Click here to email us" href="mailto:tygwynfishguard@gmail.com">tygwynfishguard@gmail.com</a>
+                    </address>
+                    <hr />
+                    <p>Alternatively, you can check our availability and make a booking on the following websites:</p>
+                    <ul>
+                        <li>
+                            <a title="Ty Gwyn listed on Owners Direct" href="http://www.ownersdirect.co.uk/wales/W964.htm">Owners Direct</a>
+                        </li>
+                        <li>
+                            <a title="Ty Gwyn listed on Holiday Lettings" href="http://www.holidaylettings.co.uk/rentals/fishguard/545122">Holiday Lettings</a>
+                        </li>
+                        <li>
+                            <a title="Ty Gwyn listed on Trip Advisor" href="http://www.tripadvisor.co.uk/VacationRentalReview-g316015-d4500184-Ty_Gwyn_Fishguard-Fishguard_Pembrokeshire_Wales.html">Trip Advisor</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="span4">
+                    <img title="The woodburner is perfect for a cosy Winter's night in" alt="A traditional woodburner in Ty Gwyn cottage" class="img-rounded" src="assets/cottage/woodburner_closeup.jpg" />
+                </div>
+            </div>
+            <!-- End page-specific content -->
 <?php include("partials/foot.php"); ?>
